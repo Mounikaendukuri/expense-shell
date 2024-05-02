8cc94b51af43659388ba3c53cac8a60e7a663957
Mounika <mounikaendukuri03@gmail.com>
2024-05-02 18:37:38 +0200

    installation_mysql

--- backend.sh
+++ backend.sh
@@ -9,7 +9,7 @@ LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.LOG
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N=="\e[0m"
+N="\e[0m"
 
 echo "please enter the root password"
 read mysql_root_password
@@ -27,47 +27,48 @@ VALIDATE() {
 if [ $USERID -ne 0 ]
 then 
     echo "you are not the root user"
-    exit1
+    exit 1
 else    
     echo "you are root user"
 fi
 
+
 dnf module disable nodejs -y &>>LOGFILE
-VALIDATE $? "Disable the old version"
+VALIDATE $? " Disable the old version "
 
 dnf module enable nodejs:20 -y &>>LOGFILE
-VALIDATE $? "enable  the new version in system"
+VALIDATE $? " enable  the new version in system "
 
 dnf install nodejs -y &>>LOGFILE
-VALIDATE $? "install the new version"
+VALIDATE $? " install the new version "
 
 id expense &>>LOGFILE #id expense will give whether the expense user has been added/not
 if [ $? -ne 0 ]
 then
     useradd expense &>>LOGFILE 
-    VALIDATE $? "creating expense user"
+    VALIDATE $? " creating expense user "
 else
-    echo "user expense already created"
+    echo " user expense already created "
 fi
 
 mkdir -p /app &>>LOGFILE #-p is used when app directory already created it will skip without throwing error
-VALIDATE $? "creating app directory"
+VALIDATE $? " creating app directory "
 
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOGFILE
-VALIDATE $? "download the backend code"
+VALIDATE $? " download the backend code "
 
 cd /app
 rm -rf /app/*
 unzip /tmp/backend.zip
-VALIDATE $? "Extracted the backend code"
+VALIDATE $? " Extracted the backend code "
 
 npm install &>>LOGFILE
-VALIDATE $? "installing node js dependencies"
+VALIDATE $? " installing node js dependencies "
 
 # (vim /etc/systemd/system/backend.service) we are not using vim becz shell script wont use vim is for manual so we are giving the absolute path for the backend file
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemmd/system/backend.service &>>LOGFILE
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>LOGFILE
 
 VALIDATE $? "copied backend service"
 
